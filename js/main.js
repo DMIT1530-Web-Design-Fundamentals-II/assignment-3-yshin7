4295ac885cedd6c4e18a813f106297daa82966bf
yshin7 <snsla10@gmail.com>
2024-08-15 13:47:00 -0600

    Removed js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const toggleBtn = document.querySelector('.toggle');
-  const navContent = document.querySelector('.nav-content');
-
-  toggleBtn.addEventListener('click', function() {
-      navContent.classList.toggle('active');
-  });
-});
