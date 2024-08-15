b3d5b02d280ad17f5404053cd9067a5dc840c544
yshin7 <snsla10@gmail.com>
2024-08-15 13:53:58 -0600

    Edited js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const toggleBtn = document.querySelector('.toggle');
   const navContent = document.querySelector('.nav-content');
 
-  toggleBtn.addEventListener('click', function() {
+  toggleBtn.addEventListener('mouseover', function() {
       navContent.classList.toggle('active');
   });
 });
